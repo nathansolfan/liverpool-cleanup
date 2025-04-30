d447cfbbeb61edd304ba3e14645f2b60d8f1bf2f
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-05-01 00:01:11 +0100

    updated

--- database/seeders/CleanupAreaSeeder.php
+++ database/seeders/CleanupAreaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class CleanupAreaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Make sure we have at least one user
+        $user = User::first() ?? User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        // Add some test cleanup areas around Liverpool
+        $areas = [
+            'title' => 'Sefton Park Litter',
+            'description' => 'Excessive litter around lake',
+            'latitude' => 53.3819,
+            'longitude' => -2.9428,
+            'severity' => 'medium',
+            'status' => 'reported',
+        ];
+
     }
 }
