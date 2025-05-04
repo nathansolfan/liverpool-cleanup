4d14cf2d921809232894e727a52682038ff3e47b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-05-04 22:08:36 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -5,38 +5,26 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// default / to welcome laravel page
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+// Public routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/map-test', function () {
-    return view('map-test');
-});
+// Individual cleanup areas are viewable by public
+Route::get('/cleanup-areas/{cleanupArea}', [CleanupAreaController::class, 'show'])->name('cleanup-areas.show');
 
-Route::get('/google-map-test', function () {
-    return view('google-map-test');
-});
-
-// Dashboard from Breeze
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-// Profile routes from Breeze
+// Authenticated routes
 Route::middleware('auth')->group(function () {
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-// Anyone can see these routes
-Route::get('/cleanup-areas', [CleanupAreaController::class, 'index'])->name('cleanup-areas.index');
-
-// Pages only logged-in users can access
-Route::middleware('auth')->group(function () {
+    // Cleanup Areas management (requires auth)
+    Route::get('/cleanup-areas', [CleanupAreaController::class, 'index'])->name('cleanup-areas.index');
     Route::get('/cleanup-areas/create', [CleanupAreaController::class, 'create'])->name('cleanup-areas.create');
     Route::post('/cleanup-areas', [CleanupAreaController::class, 'store'])->name('cleanup-areas.store');
     Route::get('/cleanup-areas/{cleanupArea}/edit', [CleanupAreaController::class, 'edit'])->name('cleanup-areas.edit');
@@ -44,11 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/cleanup-areas/{cleanupArea}', [CleanupAreaController::class, 'destroy'])->name('cleanup-areas.destroy');
 });
 
-Route::get('/cleanup-areas/{cleanupArea}', [CleanupAreaController::class, 'show'])->name('cleanup-areas.show');
-
-
-
-// API ROUTES
+// API routes
 Route::prefix('api')->group(function () {
     Route::get('/cleanup-areas', [CleanupAreaController::class, 'apiIndex'])->name('api.cleanup-areas');
 });
