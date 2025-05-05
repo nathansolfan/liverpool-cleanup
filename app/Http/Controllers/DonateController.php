70e4800a20ed2f23a4fb63f0e17026ed60e60ddd
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-05-05 12:46:44 +0100

    update

--- app/Http/Controllers/DonateController.php
+++ app/Http/Controllers/DonateController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class DonateController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('donate.index');
+    }
+
+    public function donate(Request $request)
+    {
+        $validated = $request->validate([
+            'amount' => 'required|numeric|min:1',
+            'frequency' => 'required|in:one-time,monthly',
+            'payment_method' => 'required|in:credit_card,paypal',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email'
+        ]);
+    }
 }
