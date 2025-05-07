84a011b5f73480099c26b1e9eb1f89faf27f78e7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-05-07 14:00:57 +0100

    updated Donation model

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Donation extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'user_id',
+        'amount',
+        'frequency',
+        'payment_method',
+        'name',
+        'email',
+        'stripe_payment_id',
+        'status',
+    ];
+
+    /**
+     * Get the user that made the donation.
+     */
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
